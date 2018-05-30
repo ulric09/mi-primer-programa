5e0981bb1181da0570fee003a69250902cb48e8f
Ulric09 <ulric9_93@hotmail.com>
2018-05-30 13:29:57 +0100

    probando la funcion max

---
+++ mayor_de_tres.py
@@ -0,0 +1,7 @@
+primer_numero = int(input("primer_numero: "))
+segundo_numero = int(input("segundo_numero: "))
+tercer_numero = int(input("tercer_numero: "))
+
+resultado = max(primer_numero, segundo_numero, tercer_numero)
+
+print(" el numero mas grande es {}".format(resultado))
