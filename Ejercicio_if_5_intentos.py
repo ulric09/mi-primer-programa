25cc755bf118189a5359fe944885f2c13ecf72da
Ulric09 <ulric9_93@hotmail.com>
2018-05-30 13:50:14 +0100

    Ejercicio de 5 if's

---
+++ Ejercicio_if_5_intentos.py
@@ -0,0 +1,36 @@
+number_to_guess = 9
+
+
+user_number = int(input("adivina un numero entre el 1 y el 10, tienes 5 intentos: "))
+if number_to_guess == user_number:
+    print("Ole, Ole. Alegria pal body")
+else:
+    print("Prueba otra vez")
+
+
+user_number = int(input("adivina un numero entre el 1 y el 10: "))
+if number_to_guess == user_number:
+    print("Ole, Ole. Alegria pal body")
+else:
+    print("nanai, error")
+
+
+user_number = int(input("adivina un numero entre el 1 y el 10: "))
+if number_to_guess == user_number:
+    print("Ole, Ole. Alegria pal body")
+else:
+    print("Keep trying")
+
+
+user_number = int(input("adivina un numero entre el 1 y el 10: "))
+if number_to_guess == user_number:
+        print("Ole, Ole. Alegria pal body")
+else:
+        print("casi, pero no")
+
+
+user_number = int(input("adivina un numero entre el 1 y el 10: "))
+if number_to_guess == user_number:
+    print("Ole, Ole. Alegria pal body")
+else:
+    print("perdiste, parguela")
