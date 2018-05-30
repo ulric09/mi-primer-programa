f35a7daab5ad8ffd3ab84616adb79812c1ecc865
Ulric09 <ulric9_93@hotmail.com>
2018-05-30 14:02:12 +0100

    Añadiendo variables de compparacion

--- Ejercicio_if_+dificil.py
+++ Ejercicio_if_+dificil.py
@@ -1,4 +1,4 @@
-number_to_guess = 9
+number_to_guess = 5
 
 user_number = int(input("Adivina un numero entre el 1 y el 10"))
 
@@ -25,7 +25,4 @@ user_number = int(input("Adivina un numero entre el 1 y el 10"))
 if number_to_guess == user_number:
     print("has ganado la partida, maquinote")
 else:
-    if number_to_guess < user_number:
-        print("Te has pasado por arriba!!")
-    else:
-        print("Te has quedado cortito, como tu")
+    print("Eres un parguelilla, has vuelto a perder")
