1f406414d2fd9dd88f1a2e9ade9b4f651f5707d1
Ulric09 <ulric9_93@hotmail.com>
2018-05-30 13:58:04 +0100

    He añadido, más variables dependiendo de si el resultado era mayor o menor

---
+++ Ejercicio_if_+dificil.py
@@ -0,0 +1,31 @@
+number_to_guess = 9
+
+user_number = int(input("Adivina un numero entre el 1 y el 10"))
+
+if number_to_guess == user_number:
+    print("has ganado la partida, maquinote")
+else:
+    if number_to_guess < user_number:
+        print("Te has pasado por arriba!!")
+    else:
+        print("Te has quedado cortito, como tu")
+
+user_number = int(input("Adivina un numero entre el 1 y el 10"))
+
+if number_to_guess == user_number:
+    print("has ganado la partida, maquinote")
+else:
+    if number_to_guess < user_number:
+        print("Te has pasado por arriba!!")
+    else:
+        print("Te has quedado cortito, como tu")
+
+user_number = int(input("Adivina un numero entre el 1 y el 10"))
+
+if number_to_guess == user_number:
+    print("has ganado la partida, maquinote")
+else:
+    if number_to_guess < user_number:
+        print("Te has pasado por arriba!!")
+    else:
+        print("Te has quedado cortito, como tu")
