1f000c6b7d5eab8c49945ee404f2cd25aa34c2b8
Ulric <ulric9_93@hotmail.com>
2018-06-05 15:31:13 +0100

    Programa que devuelve el largo de cada string en una lista

---
+++ Ejercicio1_lista_tipos.py
@@ -0,0 +1,10 @@
+
+lista_strings_inicial = ["aihcsc", "elckaljc", "io", "o", "uooo"]
+
+lista_resultado = []
+
+for strings in lista_strings_inicial:
+    lista_resultado.append(len(strings))
+
+print(lista_resultado)
+
