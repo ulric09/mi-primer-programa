35bd29bc34a085c8510120afb4b064e722f7988d
Ulric <ulric9_93@hotmail.com>
2018-06-05 16:34:07 +0100

    Programa que te multiplica todos los numeros de la lista entre si

---
+++ ejercicio2_lista_tipos.py
@@ -0,0 +1,9 @@
+lista_numeros = [1, 2, 3, 4, 5, 6]
+numero_uno = 1
+numero_dos = []
+
+for numeros in lista_numeros:
+   numero_dos = numeros
+   numero_uno = numero_uno * numero_dos
+
+print(numero_uno)
